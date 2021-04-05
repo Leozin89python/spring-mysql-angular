c4dcb3233484427f8124defdd7acdc54bf8321ba
Leozin89Python <leozin069@gmail.com>
2021-04-05 06:16:38 -0300

    employees

--- frontend/employeemanagerapp/src/app/employee.service.ts
+++ frontend/employeemanagerapp/src/app/employee.service.ts
@@ -1,3 +1,4 @@
+import { Employee } from './employee';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
@@ -11,7 +12,19 @@ export class EmployeeService {
 
     constructor(private http :HttpClient) {}
 
-    public getEmployees(): Observable<any> {
-        return this.http.get<any>(`${this.apiServiceUrl}/employee/all`)
-    }
+    public getEmployees(): Observable<Employee[]> {
+        return this.http.get<Employee[]>(`${this.apiServiceUrl}/employee/all`)
+  }
+
+    public addEmployees(employee :Employee): Observable<Employee> {
+      return this.http.post<Employee>(`${this.apiServiceUrl}/employee/add`,employee)
+  }
+
+  public updateEmployees(employee :Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServiceUrl}/employee/update`,employee)
+  }
+
+  public deleteEmployees(employeeId :number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServiceUrl}/employee/delete/${employeeId}`)
+  }
 }
